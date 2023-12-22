8494225edfe70e51f7f49d8040fc225556392d00
zeR0Tsu <zer0tsu233@gmail.com>
2023-12-22 15:51:44 +0800

    ud

--- demo copy 2.js
+++ demo copy 2.js
@@ -8,8 +8,8 @@ new class REIFUU_Plugin_demo3 extends REIFUU_Plugin {
   配置方式1
   config = {
     '主要配置': {
-    //   a: this.server.schemastery.number().default(10).max(20).min(10),
-    //   b: this.server.schemastery.string().default('嗨嗨嗨')
+      a: this.server.schemastery.number().default(10).max(20).min(10),
+      b: this.server.schemastery.string().default('嗨嗨嗨')
     }
   };
 
