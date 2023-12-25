e4a944b12989b2bb9f0cdd053dff0040ed8dd3f8
zeR0Tsu <zer0tsu233@gmail.com>
2023-12-25 16:03:10 +0800

    ud

--- src/demo3.js
+++ src/demo3.js
@@ -1,40 +1,45 @@
-new class REIFUU_Plugin_demo3 extends window.reifuuPluginCore.REIFUU_Plugin {
-  name = '测试3';
-  versions = '0.0.5';
-  depend = {
-    core: '0.0.3'
-  };
-
-  constructor() {
-    super();
-    this.plugInit(this);
-
-    // this.config = {
-    //   '主要配置': {
-    //     a: this.server.schemastery.number().default(10).max(20).min(10),
-    //     b: this.server.schemastery.string().default('嗨嗨嗨')
-    //   }
-    // };
-    this.config = {
-      '主要配置': {
-        a: this.server.schemastery.number().default(10).max(20).min(10),
-        b: this.server.schemastery.string().default('嗨嗨嗨')
-      }
+new class REIFUU_Plugin_demo3 extends window.reifuuPluginCore.REIFUU_Plugin
+{
+    name = '测试3';
+    versions = '0.0.5';
+    depend = {
+        core: '0.0.1'
     };
-    // 插件进行初始化代码
-    // 理论上插件允许多开，只要把变量定义在这个类里面就好了
 
-    /* code */
-  }
+    constructor()
+    {
+        super();
+        this.config = {
+            '主要配置': {
+                a: this.server.schemastery.number().default(10).max(20).min(10),
+                b: this.server.schemastery.string().default('嗨嗨嗨')
+            }
+        };
+        this.plugInit(this);
 
-  start() {
+        // this.config = {
+        //   '主要配置': {
+        //     a: this.server.schemastery.number().default(10).max(20).min(10),
+        //     b: this.server.schemastery.string().default('嗨嗨嗨')
+        //   }
+        // };
 
-    // 插件主代码
-    /* code */
-  }
+        // 插件进行初始化代码
+        // 理论上插件允许多开，只要把变量定义在这个类里面就好了
 
-  stop() {
-    // 插件消除影响代码
-    /* code */
-  }
+        /* code */
+    }
+
+    start()
+    {
+
+        // 插件主代码
+        /* code */
+    }
+
+    stop()
+    {
+        // 插件消除影响代码
+        /* code */
+    }
 };
