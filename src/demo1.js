80d476c17773208dfa9f7505e7b6e75fc5ec67f3
zeR0Tsu <zer0tsu233@gmail.com>
2024-01-03 14:45:33 +0800

    数组构型样式

--- src/demo1.js
+++ src/demo1.js
@@ -1,4 +1,7 @@
-new class REIFUU_Plugin_demo1 extends window.reifuuPluginCore.REIFUU_Plugin {
+import { number, string } from "schemastery";
+
+new class REIFUU_Plugin_demo1 extends window.reifuuPluginCore.REIFUU_Plugin
+{
     name = '测试';
     versions = '0.0.1';
     depend = {
@@ -6,22 +9,23 @@ new class REIFUU_Plugin_demo1 extends window.reifuuPluginCore.REIFUU_Plugin {
     };
     config = {
         '主要配置': {
-            a: this.server.schema.boolean().default(true),
-            b: this.server.schema.boolean().default(false),
-            c: this.server.schema.string(),
-            d: this.server.schema.string().role('secret')
+            // a: this.server.schema.boolean().default(true),
+            // b: this.server.schema.boolean().default(false),
+            // c: this.server.schema.string(),
+            // d: this.server.schema.string().role('secret')
         },
         '次要配置': {
             e: this.server.schema.string().pattern(/^custom$/i),
-            f: this.server.schema.array(Number),
-            g: this.server.schema.array(String),
+            f: this.server.schema.array(Number).default([1, 2, 3, 4, 5]),
+            g: this.server.schema.array(String).default(["12", "34"]),
             h: this.server.schema.button().link('test')
         }
     };
     url = "https://www.baidu.com";
     feedback = "https://www.baidu.com";
-    
-    constructor() {
+
+    constructor()
+    {
         super();
 
         this.plugInit(this);
@@ -32,18 +36,21 @@ new class REIFUU_Plugin_demo1 extends window.reifuuPluginCore.REIFUU_Plugin {
         /* code */
     }
 
-    start() {
+    start()
+    {
         // 插件主代码
         /* code */
-        
+
     }
 
-    stop() {
+    stop()
+    {
         // 插件消除影响代码
         /* code */
     }
 
-    test() {
+    test()
+    {
         console.log(this.value);
         console.log('button被点击');
     }
